b9d69122874611cee859ca756625690854548f56
HubanHubanov <huban.hubanov@gmail.com>
2024-05-23 12:31:28 +0300

    Add resources

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+// const express = require("express");
+// const router = express.Router();
+
+const router = require("express").Router();
+
+const homeController = require("./controllers/homeController");
+const movieController = require("./controllers/movieController");
+const castController = require("./controllers/castController");
+
+router.use(movieController);
+router.use(homeController);
+router.use("/cast", castController);
+
+router.get("*", (req, res) => {
+    res.redirect("/404");
+});
+
+
+  module.exports = router;
